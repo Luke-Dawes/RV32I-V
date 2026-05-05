02b75e2129a889d436c3d4ecc2a09781db0cd3cd
Luke Dawes <luke@daweshome.co.uk>
2026-05-05 09:43:23 +0100

    finished r-instructions

--- src/Instructions/R-type/Instructions.cpp
+++ src/Instructions/R-type/Instructions.cpp
@@ -3,15 +3,42 @@
 #include "../../CPU/Memory.h" //only way intellisense will work is include it twice
 
 
-void add(uint32_t rs, uint32_t rs1, uint32_t rs2) {
-	RAM[rs] = RAM[rs1] + RAM[rs2];
+void add(uint32_t rd, uint32_t rs1, uint32_t rs2) {
+	RAM[rd] = RAM[rs1] + RAM[rs2];
 }
 
-void sub(uint32_t rs, uint32_t rs1, uint32_t rs2) {
-	RAM[rs] = RAM[rs1] - RAM[rs2];
+void sub(uint32_t rd, uint32_t rs1, uint32_t rs2) {
+	RAM[rd] = RAM[rs1] - RAM[rs2];
 }
 
-void XOR(uint32_t rs, uint32_t rs1, uint32_t rs2) {
-	RAM[rs] = RAM[rs1] ^ RAM[rs2];
+void xor(uint32_t rd, uint32_t rs1, uint32_t rs2) {
+	RAM[rd] = RAM[rs1] ^ RAM[rs2];
 }
 
+void or(uint32_t rd, uint32_t rs1, uint32_t rs2) {
+	RAM[rd] = RAM[rs1] | RAM[rs2];
+}
+
+void and(uint32_t rd, uint32_t rs1, uint32_t rs2) {
+	RAM[rd] = RAM[rs1] & RAM[rs2];
+}
+
+void sll(uint32_t rd, uint32_t rs1, uint32_t rs2) {
+	RAM[rd] = RAM[rs1] << RAM[rs2];
+}
+
+void srl(uint32_t rd, uint32_t rs1, uint32_t rs2) {
+	RAM[rd] = RAM[rs1] >> RAM[rs2];
+}
+
+void sra(uint32_t rd, uint32_t rs1, uint32_t rs2) { //there a note of MSB-extends. I dont know what that means 
+	RAM[rd] = RAM[rs1] >> RAM[rs2];
+}
+
+void slt(uint32_t rd, uint32_t rs1, uint32_t rs2) {
+	RAM[rd] = (RAM[rs1] < RAM[rs2]) ? 1 : 0;
+}
+
+void sltu(uint32_t rd, uint32_t rs1, uint32_t rs2) { //theres a note of zero-extends. Dont know 
+	RAM[rd] = (RAM[rs1] < RAM[rs2]) ? 1 : 0;
+}
--- src/Instructions/R-type/Instructions.h
+++ src/Instructions/R-type/Instructions.h
@@ -1,6 +1,13 @@
 #pragma once
 #include <cstdint>
 
-void add(uint32_t rs, uint32_t rs1, uint32_t rs2);
-void sub(uint32_t rs, uint32_t rs1, uint32_t rs2);
-void XOR(uint32_t rs, uint32_t rs1, uint32_t rs2);
+void add(uint32_t rd, uint32_t rs1, uint32_t rs2);
+void sub(uint32_t rd, uint32_t rs1, uint32_t rs2);
+void xor(uint32_t rd, uint32_t rs1, uint32_t rs2);
+void or(uint32_t rd, uint32_t rs1, uint32_t rs2);
+void and(uint32_t rd, uint32_t rs1, uint32_t rs2);
+void sll(uint32_t rd, uint32_t rs1, uint32_t rs2);
+void srl(uint32_t rd, uint32_t rs1, uint32_t rs2);
+void sra(uint32_t rd, uint32_t rs1, uint32_t rs2);
+void slt(uint32_t rd, uint32_t rs1, uint32_t rs2);
+void sltu(uint32_t rd, uint32_t rs1, uint32_t rs2);
